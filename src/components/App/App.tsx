d47b7cb0b6afa6d4783d6a560efcae0cf813089d
Garik <garikgevorgyan.rap@mail.ru>
2025-04-21 00:05:37 +0400

    feat: add eruda for debugging

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,15 @@
 
 
+import { useEffect } from "react";
 import AppRouter from "../../router/AppRouter";
+import eruda from "eruda";
 
 export const App = () => {
-
+  useEffect(() => {
+    if (process.env.NODE_ENV === "development") {
+      eruda.init();
+    }
+  }, []);
   return (
     <>
       <AppRouter />
