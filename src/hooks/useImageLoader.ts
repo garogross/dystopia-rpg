788ffc4c965a18f5c88315274403a28faa9c5395
Garik <garikgevorgyan.rap@mail.ru>
2025-04-23 09:38:04 +0400

    fix: comment filter of webp images in useImageLoader

--- src/hooks/useImageLoader.ts
+++ src/hooks/useImageLoader.ts
@@ -1,26 +1,28 @@
-import * as allImages from "../assets/images"
-import {useEffect, useState} from "react";
+import * as allImages from "../assets/images";
+import { useEffect, useState } from "react";
 import { preloadImages } from "../helpers/preloadImages";
 
-
-
 export const useImageLoader = () => {
-    const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
 
-    const imagesArr = Object.values(allImages).filter(item => item.endsWith(".webp"))
+  const imagesArr = Object.values(allImages)
+  // .filter((item) =>
+  //   item.endsWith(".webp")
+  // );
+  // ONLY FOR DEV
 
-    useEffect(() => {
-        setLoading(true)
-        preloadImages(imagesArr)
-            .then(() => {
-            })
-            .catch((err) => {
-                console.error('Failed to load images', err);
-            }).finally(() => {
-            setLoading(false)
-        });
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+  useEffect(() => {
+    setLoading(true);
+    preloadImages(imagesArr)
+      .then(() => {})
+      .catch((err) => {
+        console.error("Failed to load images", err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-    return loading
-}
+  return loading;
+};
