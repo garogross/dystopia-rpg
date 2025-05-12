f815c5cfaabf13bf253dd9cc42be63d7aba75b16
Garik <garikgevorgyan.rap@mail.ru>
2025-05-12 20:36:20 +0400

    fix: move eruda initialization outside of development check

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,3 @@
-
-
 import { useEffect } from "react";
 import AppRouter from "../../router/AppRouter";
 import eruda from "eruda";
@@ -7,8 +5,8 @@ import eruda from "eruda";
 export const App = () => {
   useEffect(() => {
     if (process.env.NODE_ENV === "development") {
-      eruda.init();
     }
+    eruda.init();
   }, []);
   return (
     <>
