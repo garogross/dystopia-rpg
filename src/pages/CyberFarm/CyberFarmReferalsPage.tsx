7c059b22231089ac5a246d3ef49bc54d413b3a82
Garik <garikgevorgyan.rap@mail.ru>
2025-06-18 21:08:04 +0400

    refactor: fix CyberFarmReferalsPage name

---
+++ src/pages/CyberFarm/CyberFarmReferalsPage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Referals from "../../components/Referals/Referals";
+
+const CyberFarmReferalsPage = () => {
+  return <Referals />;
+};
+
+export default CyberFarmReferalsPage;
