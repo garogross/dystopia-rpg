f2679fab215bd833513284d74e3d512e427e8353
Garik <garikgevorgyan.rap@mail.ru>
2025-06-25 09:54:12 +0400

    feat: Refactor LoyalityHeader and HeaderWithBackButton to include title prop and adjust styles

--- src/components/Loyality/LoyalityHeader/LoyalityHeader.tsx
+++ src/components/Loyality/LoyalityHeader/LoyalityHeader.tsx
@@ -2,19 +2,18 @@ import React from "react";
 import HeaderWithBackButton from "../../layout/HeaderWithBackButton/HeaderWithBackButton";
 import styles from "./LoyalityHeader.module.scss";
 import { useNavigate } from "react-router-dom";
-import TitleH3 from "../../layout/TitleH3/TitleH3";
 import { useAppSelector } from "../../../hooks/redux";
 import TransitionProvider, {
   TransitionStyleTypes,
 } from "../../../providers/TransitionProvider";
 import { TRANSLATIONS } from "../../../constants/TRANSLATIONS";
 
-const {titleText} = TRANSLATIONS.loyality.header
+const { titleText } = TRANSLATIONS.loyality.header;
 
 const LoyalityHeader = () => {
   const navigate = useNavigate();
   const gameInited = useAppSelector((state) => state.ui.gameInited);
-    const language = useAppSelector(state => state.ui.language)
+  const language = useAppSelector((state) => state.ui.language);
 
   return (
     <TransitionProvider
@@ -22,8 +21,10 @@ const LoyalityHeader = () => {
       style={TransitionStyleTypes.zoomOut}
       inProp={gameInited}
     >
-      <HeaderWithBackButton onClose={() => navigate(-1)} />
-      <TitleH3>{titleText[language]}</TitleH3>
+      <HeaderWithBackButton
+        onClose={() => navigate(-1)}
+        title={titleText[language]}
+      />
     </TransitionProvider>
   );
 };
