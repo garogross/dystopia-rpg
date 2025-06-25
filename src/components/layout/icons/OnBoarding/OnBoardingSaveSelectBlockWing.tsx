5758862dec13a95782b0d4463391fc8e4537cc39
Garik <garikgevorgyan.rap@mail.ru>
2025-06-25 23:07:07 +0400

    feat: Update dimensions and styles for OnBoardingSaveSelectBlock components

--- src/components/layout/icons/OnBoarding/OnBoardingSaveSelectBlockWing.tsx
+++ src/components/layout/icons/OnBoarding/OnBoardingSaveSelectBlockWing.tsx
@@ -8,13 +8,15 @@ export const OnBoardingSaveSelectBlockWing = ({
   return (
     <svg
       preserveAspectRatio={"none"}
-      viewBox="0 0 55 111"
+      width="45"
+      height="111"
+      viewBox="0 0 45 111"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       style={{ transform: rotated ? "rotateY(180deg)" : undefined }}
     >
       <path
-        d="M0 24.69L25.328 111H55L25.328 23.125L52.4771 0H25.328L0 24.69Z"
+        d="M0 24.69L25.328 111H45L15.33 23.125L42.48 0H25.328L0 24.69Z"
         fill="url(#paint0_radial_1491_11099)"
       />
       <defs>
@@ -26,8 +28,8 @@ export const OnBoardingSaveSelectBlockWing = ({
           gradientUnits="userSpaceOnUse"
           gradientTransform="translate(26.2385 55.5) rotate(90) scale(94.9666 44.8971)"
         >
-          <stop stopColor="#3D2B7E" />
-          <stop offset="0.980769" stopColor="#040010" />
+          <stop stop-color="#3D2B7E" />
+          <stop offset="0.980769" stop-color="#040010" />
         </radialGradient>
       </defs>
     </svg>
