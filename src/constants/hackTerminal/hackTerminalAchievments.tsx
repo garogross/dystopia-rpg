a07f9b68110eb9a7029302ea5567080d9210a6be
Garik <garikgevorgyan.rap@mail.ru>
2025-07-04 23:05:08 +0400

    feat: refactor Achievments component to accept props for achievements, details, and settings

--- src/constants/hackTerminal/hackTerminalAchievments.tsx
+++ src/constants/hackTerminal/hackTerminalAchievments.tsx
@@ -22,89 +22,91 @@ import {
   AchievmentWinInRow4Level,
   AchievmentWinInRow5Level,
 } from "../../components/layout/icons/Achievments/hackTerminal/WinInRow";
+import { AchievmentDetailsType } from "../../types/Achievments/AchievmentDetailsType";
 
 const { win, winInRow, saveWin } = TRANSLATIONS.hackTerminal.achievments;
 
-export const HACK_TERMINAL_ACHIEVMENTS = {
-  [EHackTerminalAchievments.WIN]: [
-    {
-      icon: <AchievmentWin1Level />,
-      title: win.level1,
-      description: win.desc1,
-    },
-    {
-      icon: <AchievmentWin2Level />,
-      title: win.level2,
-      description: win.desc2,
-    },
-    {
-      icon: <AchievmentWin3Level />,
-      title: win.level3,
-      description: win.desc3,
-    },
-    {
-      icon: <AchievmentWin4Level />,
-      title: win.level4,
-      description: win.desc4,
-    },
-    {
-      icon: <AchievmentWin5Level />,
-      title: win.level5,
-      description: win.desc5,
-    },
-  ],
-  [EHackTerminalAchievments.SAVE_WIN]: [
-    {
-      icon: <AchievmentSaveWin1Level />,
-      title: saveWin.level1,
-      description: saveWin.desc1,
-    },
-    {
-      icon: <AchievmentSaveWin2Level />,
-      title: saveWin.level2,
-      description: saveWin.desc2,
-    },
-    {
-      icon: <AchievmentSaveWin3Level />,
-      title: saveWin.level3,
-      description: saveWin.desc3,
-    },
-    {
-      icon: <AchievmentSaveWin4Level />,
-      title: saveWin.level4,
-      description: saveWin.desc4,
-    },
-    {
-      icon: <AchievmentSaveWin5Level />,
-      title: saveWin.level5,
-      description: saveWin.desc5,
-    },
-  ],
-  [EHackTerminalAchievments.WIN_IN_ROW]: [
-    {
-      icon: <AchievmentWinInRow1Level />,
-      title: winInRow.level1,
-      description: winInRow.desc1,
-    },
-    {
-      icon: <AchievmentWinInRow2Level />,
-      title: winInRow.level2,
-      description: winInRow.desc2,
-    },
-    {
-      icon: <AchievmentWinInRow3Level />,
-      title: winInRow.level3,
-      description: winInRow.desc3,
-    },
-    {
-      icon: <AchievmentWinInRow4Level />,
-      title: winInRow.level4,
-      description: winInRow.desc4,
-    },
-    {
-      icon: <AchievmentWinInRow5Level />,
-      title: winInRow.level5,
-      description: winInRow.desc5,
-    },
-  ],
-};
+export const HACK_TERMINAL_ACHIEVMENTS: AchievmentDetailsType<EHackTerminalAchievments> =
+  {
+    [EHackTerminalAchievments.WIN]: [
+      {
+        icon: <AchievmentWin1Level />,
+        title: win.level1,
+        description: win.desc1,
+      },
+      {
+        icon: <AchievmentWin2Level />,
+        title: win.level2,
+        description: win.desc2,
+      },
+      {
+        icon: <AchievmentWin3Level />,
+        title: win.level3,
+        description: win.desc3,
+      },
+      {
+        icon: <AchievmentWin4Level />,
+        title: win.level4,
+        description: win.desc4,
+      },
+      {
+        icon: <AchievmentWin5Level />,
+        title: win.level5,
+        description: win.desc5,
+      },
+    ],
+    [EHackTerminalAchievments.SAVE_WIN]: [
+      {
+        icon: <AchievmentSaveWin1Level />,
+        title: saveWin.level1,
+        description: saveWin.desc1,
+      },
+      {
+        icon: <AchievmentSaveWin2Level />,
+        title: saveWin.level2,
+        description: saveWin.desc2,
+      },
+      {
+        icon: <AchievmentSaveWin3Level />,
+        title: saveWin.level3,
+        description: saveWin.desc3,
+      },
+      {
+        icon: <AchievmentSaveWin4Level />,
+        title: saveWin.level4,
+        description: saveWin.desc4,
+      },
+      {
+        icon: <AchievmentSaveWin5Level />,
+        title: saveWin.level5,
+        description: saveWin.desc5,
+      },
+    ],
+    [EHackTerminalAchievments.WIN_IN_ROW]: [
+      {
+        icon: <AchievmentWinInRow1Level />,
+        title: winInRow.level1,
+        description: winInRow.desc1,
+      },
+      {
+        icon: <AchievmentWinInRow2Level />,
+        title: winInRow.level2,
+        description: winInRow.desc2,
+      },
+      {
+        icon: <AchievmentWinInRow3Level />,
+        title: winInRow.level3,
+        description: winInRow.desc3,
+      },
+      {
+        icon: <AchievmentWinInRow4Level />,
+        title: winInRow.level4,
+        description: winInRow.desc4,
+      },
+      {
+        icon: <AchievmentWinInRow5Level />,
+        title: winInRow.level5,
+        description: winInRow.desc5,
+      },
+    ],
+  };
--- src/pages/HackTerminal/HackTerminalAchievmentsPage.tsx
+++ src/pages/HackTerminal/HackTerminalAchievmentsPage.tsx
@@ -1,7 +1,48 @@
 import React from "react";
+import Achievments from "../../components/Achievments/Achievments";
+import { HACK_TERMINAL_ACHIEVMENTS } from "../../constants/hackTerminal/hackTerminalAchievments";
+import { EHackTerminalAchievments } from "../../constants/hackTerminal/EHackTerminalAchievments";
 
 const HackTerminalAchievmentsPage = () => {
-  return <div></div>;
+  const achievments = {
+    [EHackTerminalAchievments.WIN]: {
+      count: 1,
+      level: 1,
+    },
+    [EHackTerminalAchievments.SAVE_WIN]: {
+      count: 1,
+      level: 1,
+    },
+    [EHackTerminalAchievments.WIN_IN_ROW]: {
+      count: 1,
+      level: 1,
+    },
+  };
+
+  const settings = {
+    [EHackTerminalAchievments.WIN]: {
+      desc: "lorem text",
+      levels: [2, 2, 2, 2, 2],
+      reward_per_level_hours: 1,
+    },
+    [EHackTerminalAchievments.SAVE_WIN]: {
+      desc: "lorem text",
+      levels: [2, 2, 2, 2, 2],
+      reward_per_level_hours: 1,
+    },
+    [EHackTerminalAchievments.WIN_IN_ROW]: {
+      desc: "lorem text",
+      levels: [2, 2, 2, 2, 2],
+      reward_per_level_hours: 1,
+    },
+  };
+  return (
+    <Achievments
+      achievments={achievments}
+      details={HACK_TERMINAL_ACHIEVMENTS}
+      settings={settings}
+    />
+  );
 };
 
 export default HackTerminalAchievmentsPage;
