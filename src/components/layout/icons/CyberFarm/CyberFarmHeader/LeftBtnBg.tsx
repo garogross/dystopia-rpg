9c87b32b1ce75911649d77326d3ce1e4a72d6c92
Garik <garikgevorgyan.rap@mail.ru>
2025-09-10 20:07:57 +0400

    refactor: Simplify CyberFarmHeader component and update styles for improved layout and responsiveness; remove unused icons and related code

---
+++ src/components/layout/icons/CyberFarm/CyberFarmHeader/LeftBtnBg.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+export const LeftBtnBg = () => {
+  return (
+    <svg
+      width="48"
+      height="28"
+      viewBox="0 0 48 28"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M0 0H45.857C45.857 0 48 8.47364 48 14C48 19.5264 45.857 28 45.857 28H0C0 28 2.02105 19.4899 2.02105 14C2.02105 8.51011 0 0 0 0Z"
+        fill="url(#paint0_linear_3224_24807)"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_3224_24807"
+          x1="24"
+          y1="0"
+          x2="24"
+          y2="28"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#2A1E54" />
+          <stop offset="1" stop-color="#5D42BA" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+};
---
+++ src/components/layout/icons/CyberFarm/CyberFarmHeader/RightBtnBg.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+export const RightBtnBg = () => {
+  return (
+    <svg
+      width="48"
+      height="28"
+      viewBox="0 0 48 28"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M48 0H2.14301C2.14301 0 0 8.47364 0 14C0 19.5264 2.14301 28 2.14301 28H48C48 28 45.9789 19.4899 45.9789 14C45.9789 8.51011 48 0 48 0Z"
+        fill="url(#paint0_linear_3224_24808)"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_3224_24808"
+          x1="24"
+          y1="0"
+          x2="24"
+          y2="28"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#2A1E54" />
+          <stop offset="1" stop-color="#5D42BA" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+};
