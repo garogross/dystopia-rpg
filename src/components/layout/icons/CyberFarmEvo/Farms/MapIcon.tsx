185ca144ed4de98b24b809fc529c88c3761163e2
Garik <garikgevorgyan.rap@mail.ru>
2025-09-28 13:43:32 +0400

    fix: update stroke properties in BlockedSlotIcon and MapIcon components for consistency; replace stroke attributes with camelCase notation

--- src/components/layout/icons/CyberFarmEvo/Farms/MapIcon.tsx
+++ src/components/layout/icons/CyberFarmEvo/Farms/MapIcon.tsx
@@ -12,9 +12,9 @@ export const MapIcon = () => {
       <path
         d="M8.5 3.64706H8.50833M6 16L1 13.3529V1.88235L2.66667 2.76471M6 16L11 13.3529M6 16V10.7059M11 13.3529L16 16V4.52941L14.3333 3.64706M11 13.3529V10.7059M11 3.82353C11 5.38292 9.75 6.64706 8.5 8.05882C7.25 6.64706 6 5.38292 6 3.82353C6 2.26414 7.11925 1 8.5 1C9.88075 1 11 2.26414 11 3.82353Z"
         stroke="#C6B7FF"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
