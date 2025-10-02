1b8ce655a2f0c6c14021276ead0b9a673b6d7a6a
Garik <garikgevorgyan.rap@mail.ru>
2025-10-02 15:39:40 +0400

    refactor: streamline CyberFarmRatings component by removing unused data processing and styles; enhance Table component with active item highlighting

--- src/components/CyberFarm/CyberFarmRatings/CyberFarmRatings.tsx
+++ src/components/CyberFarm/CyberFarmRatings/CyberFarmRatings.tsx
@@ -46,31 +46,17 @@ const CyberFarmRatings = () => {
   const language = useAppSelector((state) => state.ui.language);
   const [searchQuery, setSearchQuery] = useState("");
 
-  const data = ratings
-    ?.map((item, index) => ({
-      id: item.user_id,
-      index: index + 1,
-      name: item.name,
-      builings: item.structures_value,
-      technology: 0,
-      general: item.total_value,
-    }))
-    .filter(
-      (item) =>
-        item.id !== tgid &&
-        item.name.toLowerCase().includes(searchQuery.toLowerCase())
-    );
+  const data = ratings?.map((item, index) => ({
+    id: item.user_id,
+    index: index + 1,
+    name: item.name,
+    builings: item.structures_value,
+    technology: 0,
+    general: item.total_value,
+  }));
+
   const curUserIndex = ratings?.findIndex((item) => item.user_id === tgid);
-  const curUserData =
-    ratings && curUserIndex !== undefined && curUserIndex !== -1
-      ? {
-          index: curUserIndex + 1,
-          name: ratings?.[curUserIndex].name,
-          builings: ratings?.[curUserIndex].structures_value,
-          technology: 0,
-          general: ratings?.[curUserIndex].total_value,
-        }
-      : null;
+
   useEffect(() => {
     dispatch(getRatingsList());
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -88,27 +74,12 @@ const CyberFarmRatings = () => {
       {data && (
         <div className={styles.cyberFarmRatings__tableWrapper}>
           <Table
-            className={styles.cyberFarmRatings__table}
             columnsTemplate={columnsTemplate}
             headers={headers}
             data={data}
             cols={cols}
+            activeItemIndex={curUserIndex}
           />
-          {curUserData && (
-            <div
-              className={styles.cyberFarmRatings__ownCol}
-              style={{ gridTemplateColumns: columnsTemplate }}
-            >
-              {cols.map((item) => (
-                <p
-                  key={item.key}
-                  className={styles.cyberFarmRatings__ownColText}
-                >
-                  {curUserData[item.key]}
-                </p>
-              ))}
-            </div>
-          )}
         </div>
       )}
     </div>
