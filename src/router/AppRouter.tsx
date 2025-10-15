0bdbe0b5ba49f03881b604579964496e8ac4279f
Garik <garikgevorgyan.rap@mail.ru>
2025-10-15 13:12:09 +0400

    refactor: implement lazy loading for route components and add Suspense with AppLoader for improved loading experience

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,33 +1,47 @@
-import { Fragment } from "react";
+import { Fragment, Suspense, useState } from "react";
 import { Route, Routes, Outlet } from "react-router-dom";
 
 import { routes } from "./path";
+import AppLoader from "../components/AppLoader/AppLoader";
+import { ESplashTypes } from "../constants/ESplashTypes";
 
 function AppRouter() {
+  const [timerFinished, setTimerFinished] = useState(true);
   return (
-    <Routes>
-      {routes.map(({ path, component, children, indexComponent }, index) => (
-        <Fragment key={index}>
-          {children ? (
-            <Route
-              path={path}
-              element={indexComponent ? component : <Outlet />}
-            >
-              <Route index element={indexComponent || component} />
-              {children.map((child, childIndex) => (
-                <Route
-                  path={child.path}
-                  element={child.component}
-                  key={childIndex}
-                />
-              ))}
-            </Route>
-          ) : (
-            <Route path={path} element={component} />
-          )}
-        </Fragment>
-      ))}
-    </Routes>
+    <Suspense
+      fallback={
+        <AppLoader
+          loading={true}
+          timerFinished={timerFinished}
+          setTimerFinished={setTimerFinished}
+          type={ESplashTypes.CYBERFARM}
+        />
+      }
+    >
+      <Routes>
+        {routes.map(({ path, component, children, indexComponent }, index) => (
+          <Fragment key={index}>
+            {children ? (
+              <Route
+                path={path}
+                element={indexComponent ? component : <Outlet />}
+              >
+                <Route index element={indexComponent || component} />
+                {children.map((child, childIndex) => (
+                  <Route
+                    path={child.path}
+                    element={child.component}
+                    key={childIndex}
+                  />
+                ))}
+              </Route>
+            ) : (
+              <Route path={path} element={component} />
+            )}
+          </Fragment>
+        ))}
+      </Routes>
+    </Suspense>
   );
 }
 
